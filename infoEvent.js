6c51950da7bcfbe200e1724f3b9ecb4633340c1f
toka601 <marth601lucina@icloud.com>
2026-08-30 00:33:54 +0900

    Update infoEvent.js

--- infoEvent.js
+++ infoEvent.js
@@ -6,6 +6,7 @@
  * ・点数表(ツモ)
  */
 
+// ダブ1「用語解説」のボタン設定
 const descButtons = document.querySelectorAll(".descButton");
 const descContents = document.querySelectorAll(".descContent");
 
@@ -25,6 +26,39 @@ descButtons.forEach(button => {
             }
         }
     });
+});
+
+
+// タブ2「点数表(ロン)」のタブボタン設定
+const ronTabButtons = document.querySelectorAll(".ronScoreButton");
+const ronTabContents = document.querySelectorAll(".ronScoreList");
+
+ronTabButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        ronTabButtons.forEach(button => button.classList.remove("active"));
+        ronTabContents.forEach(content => content.classList.remove("active"));
+
+        button.classList.add("active");
+        const tabId = button.dataset.tab;
+
+        document.getElementById(tabId).classList.add("active");
+    })
+});
+
+// タブ3「点数表(ツモ)」のタブボタン設定
+const tsumoTabButtons = document.querySelectorAll(".tsumoScoreButton");
+const tsumoTabContents = document.querySelectorAll(".tsumoScoreList");
+
+tsumoTabButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        tsumoTabButtons.forEach(button => button.classList.remove("active"));
+        tsumoTabContents.forEach(content => content.classList.remove("active"));
+
+        button.classList.add("active");
+        const tabId = button.dataset.tab;
+
+        document.getElementById(tabId).classList.add("active");
+    })
 });
 
 
