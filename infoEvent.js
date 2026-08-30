ffbe6da167d148b570641d9b2724706113442538
toka601 <marth601lucina@icloud.com>
2026-08-30 13:07:52 +0900

    Update infoEvent.js

--- infoEvent.js
+++ infoEvent.js
@@ -29,63 +29,6 @@ descButtons.forEach(button => {
 });
 
 
-// タブ2「点数表(ロン)」のタブボタン設定
-const ronTabButtons = document.querySelectorAll(".ronScoreButton");
-const ronTabContents = document.querySelectorAll(".ronScoreList");
-
-ronTabButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        ronTabButtons.forEach(button => button.classList.remove("active"));
-        ronTabContents.forEach(content => content.classList.remove("active"));
-
-        button.classList.add("active");
-        const tabId = button.dataset.tab;
-
-        document.getElementById(tabId).classList.add("active");
-    })
-});
-
-// タブ3「点数表(ツモ)」のタブボタン設定
-const tsumoTabButtons = document.querySelectorAll(".tsumoScoreButton");
-const tsumoTabContents = document.querySelectorAll(".tsumoScoreList");
-
-tsumoTabButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        tsumoTabButtons.forEach(button => button.classList.remove("active"));
-        tsumoTabContents.forEach(content => content.classList.remove("active"));
-
-        button.classList.add("active");
-        const tabId = button.dataset.tab;
-
-        document.getElementById(tabId).classList.add("active");
-    })
-});
-
-
-// タブと対応ページ
-const tabButtons = document.querySelectorAll(".tabButton");
-const tabContents = document.querySelectorAll(".tabContent");
-
-
-tabButtons.forEach(button => {
-
-    // タブ切り替え
-    button.addEventListener("click", () => {
-
-        // すべてのタブと内容を非選択
-        tabButtons.forEach(button => button.classList.remove("active") );
-        tabContents.forEach(content => content.classList.remove("active") );
-
-        // 押されたタブを選択
-        button.classList.add("active");
-
-        const tabId = button.dataset.tab;
-
-        document.getElementById(tabId).classList.add("active");
-    });
-
-});
-
 const popup = document.getElementById("popup");
 
 document.getElementById("imageButton").addEventListener("click", () => {
