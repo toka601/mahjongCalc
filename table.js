778092d6d8f539152fd28b8bfde00070e6efbef4
toka601 <marth601lucina@icloud.com>
2026-09-01 15:32:38 +0900

    Update table.js

--- table.js
+++ table.js
@@ -1,6 +1,6 @@
-document.getElementById("mentsuTable")
-        .querySelectorAll("select")
-        .forEach(select => {
+const tableCells = document.getElementById("mentsuTable").querySelectorAll("select");
+
+tableCells.forEach(select => {
     select.addEventListener("change", () => {
         if (select.value !== "0") {
             select.parentElement.style.backgroundColor = "rgb(85, 139, 255)";
